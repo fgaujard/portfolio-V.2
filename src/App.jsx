eb4c7a8246f72cf72a8a36d9f90980685a5b8504
Flavien G <44913995+fgaujard@users.noreply.github.com>
2024-05-30 17:51:28 +0200

    Helmet SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import Navbar from "./components/Navbar";
 
 import Projects from "./layouts/Projects";
@@ -5,12 +7,46 @@ import Home from "./layouts/Home";
 import Skills from "./layouts/Skills";
 import Contact from "./layouts/Contact";
 
+import snapshot from "./assets/snapshot.png";
+
 import "./scss/main.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
+  const schema = {
+    "@context": "http://schema.org",
+    "@type": "Person",
+    name: "Flavien Gaujard",
+    url: "https://flavien-gaujard.com",
+    image: snapshot,
+    sameAs: [
+      "https://linkedin.com/in/flavien-gaujard",
+      "https://github.com/fgaujard",
+    ],
+    jobTitle: "Web Developer",
+    address: {
+      "@type": "PostalAddress",
+      addressLocality: "Chartres",
+      addressRegion: "Centre-Val de Loire",
+      addressCountry: "France",
+    },
+  };
+
   return (
     <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Explore the portfolio of Flavien Gaujard, a web developer based in Chartres, France. Discover his projects and skills in web development."
+        />
+        <meta
+          name="keywords"
+          content="web development, web developer, portfolio, Flavien Gaujard, Chartres, France, React, Javascript, CSS, HTML, Sassy, SCSS, French developer."
+        />
+        <link rel="canonical" href="https://flavien-gaujard.fr" />
+        <script type="application/ld+json">{JSON.stringify(schema)}</script>
+      </Helmet>
+
       <Navbar />
 
       <main className="app-body">
